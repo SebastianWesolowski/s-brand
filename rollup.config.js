6865fe0635e470e3b877f6db2c3288b685498888
Sebastian Wesołowski <Sebastian.wesolowski.sw@gmail.com>
2022-01-03 14:13:37 +0100

    chore: build
    Improve build process

---
+++ rollup.config.js
@@ -0,0 +1,81 @@
+import { babel } from "@rollup/plugin-babel";
+import resolve from "@rollup/plugin-node-resolve";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import { terser } from "rollup-plugin-terser";
+import dts from "rollup-plugin-dts";
+import commonjs from "@rollup/plugin-commonjs";
+import scss from "rollup-plugin-scss";
+
+import packageJson from "./package.json";
+
+const external = [];
+
+const plugins = [
+  peerDepsExternal(),
+  resolve({
+    browser: true,
+  }),
+
+  commonjs(),
+  babel({
+    babelHelpers: "bundled",
+    comments: false,
+    compact: true,
+    minified: true,
+    exclude: "node_modules/**",
+    presets: ["@babel/preset-react"],
+  }),
+  scss({
+    output: "lib/main.css",
+    outputStyle: "compressed",
+  }),
+  terser(),
+];
+
+const config = [
+  {
+    input: "./distTS/index.d.ts",
+    output: [
+      {
+        file: packageJson.types,
+        format: "es",
+        compact: true,
+      },
+    ],
+    plugins: [
+      dts(),
+      scss({
+        output: false,
+      }),
+    ],
+    external,
+  },
+  {
+    input: "./distTS/index.js",
+    output: [
+      {
+        dir: "./lib/es",
+        format: "es",
+        compact: true,
+        preserveModules: true,
+        preserveModulesRoot: "./distTS",
+      },
+    ],
+    plugins,
+    external,
+  },
+  {
+    input: "./distTS/index.js",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        compact: true,
+      },
+    ],
+    plugins,
+    external,
+  },
+];
+
+export default config;
---
+++ src/themes/BetaBlog/BetaBlog.ts
@@ -0,0 +1,13 @@
+// Create a theme instance.
+export const theme = {
+  palette: {
+    primary: {
+      main: "#556cd6",
+    },
+    secondary: {
+      main: "#19857b",
+    },
+  },
+};
+
+// https://medium.com/@abdurakhimov.sardor/how-to-use-and-customize-material-ui-version-5-with-styled-components-295e62562e61
